87c824f222da1d2ca2437d78c3f0631eb2fc7c35
Ilja Kartašov <ik@lowenware.com>
2022-02-20 17:28:05 +0100

    Add possibility for Id structure to be based on Rust TypeId

--- dotrix_core/src/id.rs
+++ dotrix_core/src/id.rs
@@ -1,14 +1,22 @@
 //! Assets identifiers
 use std::{
+    any::TypeId,
     fmt::Debug,
     hash::{Hash, Hasher},
     marker::PhantomData,
 };
 
+/// Enumeration of types of actual Id value
+#[derive(Debug, Hash, Copy, Clone, Eq, PartialEq)]
+pub enum ValueOf {
+    TypeId(TypeId),
+    Number(u64),
+}
+
 /// Asset identifier
 pub struct Id<T> {
     /// Actual identifier value
-    pub id: u64,
+    pub id: ValueOf,
     phantom: PhantomData<T>,
 }
 
@@ -16,15 +24,38 @@ impl<T> Id<T> {
     /// Constructs an asset identifier from u64
     pub fn new(id: u64) -> Self {
         Self {
-            id,
+            id: ValueOf::Number(id),
             phantom: PhantomData,
         }
     }
 
     /// Checks if id is null
     pub fn is_null(&self) -> bool {
-        self.id == 0
+        self.id == ValueOf::Number(0)
     }
+}
+
+impl<T> From<TypeId> for Id<T> {
+    fn from(value: TypeId) -> Self {
+        Self {
+            id: ValueOf::TypeId(value),
+            phantom: PhantomData,
+        }
+    }
+}
+
+impl<T> From<u64> for Id<T> {
+    fn from(value: u64) -> Self {
+        Self {
+            id: ValueOf::Number(value),
+            phantom: PhantomData,
+        }
+    }
+}
+
+/// Abstraction for type-based IP getters
+pub trait OfType {
+    fn of<T: std::any::Any>() -> Self;
 }
 
 impl<T> Eq for Id<T> {}
@@ -44,7 +75,7 @@ impl<T> Hash for Id<T> {
 impl<T> Debug for Id<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
         let of_type = std::any::type_name::<T>().split("::").last().unwrap();
-        write!(f, "[Id<{}>: {}]", of_type, self.id)
+        write!(f, "[Id<{}>: {:?}]", of_type, self.id)
     }
 }
 
