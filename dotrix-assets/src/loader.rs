4d149c547cab2276f0166fd3b6694bf67bbb522a
Ilja Kartašov <ik@lowenware.com>
2022-07-22 11:20:00 +0200

    Add prototype of assets loading

--- dotrix-assets/src/loader.rs
+++ dotrix-assets/src/loader.rs
@@ -1,13 +1,16 @@
-use crate::Resource;
+use crate::Asset;
 
+#[derive(Debug, Eq, PartialEq, Clone)]
 pub enum LoadError {
-    OpenFile,
-    ReadFile,
+    NotSupported,
+    OnOpen,
+    OnParse(String),
 }
 
 pub trait Loader: std::any::Any + Send + 'static {
     /// Returns true if loader is able to import file of that extension
     fn can_load(&self, file_extension: &str) -> bool;
-    /// Tries to load assets from file
-    fn load(&self, path: &std::path::Path) -> Result<Resource, LoadError>;
+    /// Tries to load assets from file buffer
+    /// Returns pair of namespace and imported asset
+    fn load(&self, name: &str, buffer: Vec<u8>) -> Vec<(u64, Box<dyn Asset>)>;
 }
--- dotrix-assets/src/tasks.rs
+++ dotrix-assets/src/tasks.rs
@@ -1,19 +1,57 @@
-use crate::{Assets, Bundle, File, Resource};
+use crate::{Assets, Bundle, File, LoadError, Resource};
 use dotrix_core as dotrix;
+use dotrix_log as log;
+use std::collections::HashMap;
 
 #[derive(Default)]
 pub struct LoadTask {}
 
 impl dotrix::Task for LoadTask {
-    type Context = (dotrix::Any<File>,);
+    type Context = (dotrix::Any<File>, dotrix::Ref<Assets>);
     type Provides = Bundle;
 
-    fn run(&mut self, (file,): Self::Context) -> Self::Provides {
+    fn run(&mut self, (file, assets): Self::Context) -> Self::Provides {
+        use std::io::Read;
+
+        let file_name = file
+            .path
+            .file_stem()
+            .map(|n| n.to_str().unwrap())
+            .unwrap()
+            .to_string();
+
+        let extension = file
+            .path
+            .extension()
+            .map(|e| e.to_str().unwrap())
+            .unwrap_or("")
+            .to_string();
+
+        let mut list = None;
+
+        for loader in assets.loaders() {
+            if loader.can_load(&extension) {
+                if let Ok(mut fs_file) = std::fs::File::open(&file.path) {
+                    let mut buffer = Vec::with_capacity(file.size);
+                    if fs_file.read_to_end(&mut buffer).is_ok() {
+                        list = Some(loader.load(&file_name, buffer));
+                    } else {
+                        log::error!("Could not read file {:?}", &file.path);
+                    }
+                } else {
+                    log::error!("Could not open file {:?}", &file.path);
+                }
+                break;
+            }
+        }
+
+        if list.is_none() {
+            println!("No loader found for {:?}", &file.path);
+        }
+
         Bundle {
             path: file.path.clone(),
-            assets: Vec::new(),
-            version: 0,
-            last_modified: None,
+            assets: list.unwrap_or_else(Vec::new),
         }
     }
 }
@@ -22,27 +60,66 @@ impl dotrix::Task for LoadTask {
 pub struct StoreTask {}
 
 impl dotrix::Task for StoreTask {
-    type Context = (dotrix::Any<Resource>, dotrix::Mut<Assets>);
+    type Context = (dotrix::Take<Bundle>, dotrix::Mut<Assets>);
     type Provides = Resource;
 
-    fn run(&mut self, (resource, assets): Self::Context) -> Self::Provides {
-        Resource {
-            path: resource.path.clone(),
-            last_modified: resource.last_modified,
-            version: resource.version,
-            assets: Vec::new(),
+    fn run(&mut self, (mut bundle, mut assets): Self::Context) -> Self::Provides {
+        use crate::Asset;
+
+        let mut meta = Vec::<(String, String)>::with_capacity(bundle.assets.len());
+        while let Some((namespace, asset)) = bundle.assets.pop() {
+            let name = asset.name().to_string();
+            let type_name = asset.type_name().to_string();
+            assets.store_raw(namespace, asset);
+            meta.push((type_name, name));
         }
+
+        let mut resource = assets.resource(bundle.path.clone());
+        // NOTE: in theory we can remove old assets here
+
+        resource.version += 1;
+        resource.assets = meta;
+        resource.clone()
     }
 }
 
-#[derive(Default)]
 pub struct Watchdog {
     pub hot_reload: bool,
+    pub registry: HashMap<std::path::PathBuf, std::time::SystemTime>,
+}
+
+impl Default for Watchdog {
+    fn default() -> Self {
+        Self {
+            hot_reload: false,
+            registry: HashMap::new(),
+        }
+    }
 }
 
 impl dotrix::Task for Watchdog {
     type Context = (dotrix::Ref<Assets>, dotrix::Ref<dotrix::Tasks>);
     type Provides = ();
 
-    fn run(&mut self, (assets, tasks): Self::Context) -> Self::Provides {}
+    fn run(&mut self, (assets, tasks): Self::Context) -> Self::Provides {
+        for resource in assets.resources() {
+            if let Ok(metadata) = std::fs::metadata(&resource.path) {
+                if let Ok(resource_modified) = metadata.modified() {
+                    if let Some(last_modified) = self.registry.get(&resource.path) {
+                        if resource_modified == *last_modified {
+                            continue;
+                        }
+                    }
+
+                    self.registry
+                        .insert(resource.path.clone(), resource_modified);
+
+                    tasks.provide(File {
+                        path: resource.path.clone(),
+                        size: metadata.len() as usize,
+                    });
+                }
+            }
+        }
+    }
 }
